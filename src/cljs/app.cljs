89462e0e5774cb2c4eef15421a6e7ca4e8625950
Max Kreminski <maxkreminski@gmail.com>
2014-02-10 08:06:26 -0800

    Add command history and allow backwards traversal with up arrow

--- src/cljs/app.cljs
+++ src/cljs/app.cljs
@@ -1,8 +1,21 @@
 (ns app
   (:require [cljs.repl :as repl]))
 
-(def input (.getElementById js/document "input"))
 (def log (.getElementById js/document "log"))
+
+(def session
+  (atom {:history {:commands [] :cursor 0}}))
+
+(defn add-history-item! [code]
+  (let [next-cursor (count (get-in @session [:history :commands]))]
+    (swap! session update-in [:history :commands] #(conj % code))
+    (swap! session assoc-in [:history :cursor] next-cursor)))
+
+(defn prev-history-item! []
+  (let [{:keys [commands cursor]} (:history @session)]
+    (when-let [prev (get commands cursor)]
+      (swap! session update-in [:history :cursor] dec)
+      prev)))
 
 (defn log-entry [in out]
   (let [entry (.createElement js/document "div")]
@@ -19,10 +32,20 @@
 (defn key-code [ev]
   (or (.-key ev) (.-keyCode ev) (.-which ev)))
 
-(defn handle-key [ev]
-  (condp = (key-code ev)
-    13 (do (eval-print! (.-value input))
-           (set! (.-value input) ""))
-    nil))
+(def keybinds
+  {13 (fn [input]
+        (let [code (.-value input)]
+          (eval-print! code)
+          (add-history-item! code))
+        (set! (.-value input) ""))
 
-(.addEventListener input "keydown" handle-key)
+   38 (fn [input]
+        (set! (.-value input) (prev-history-item!)))})
+
+(defn handle-key [ev]
+  (when-let [keybind (keybinds (key-code ev))]
+    (let [input (.-target ev)]
+      (keybind input))))
+
+(let [input (.getElementById js/document "input")]
+  (.addEventListener input "keydown" handle-key))
