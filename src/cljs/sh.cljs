d4a6a2a4d19ea4a9f92c5c65195220cec1368436
Max Kreminski <maxkreminski@gmail.com>
2014-02-13 14:41:43 -0800

    Add sh/ls function

--- src/cljs/sh.cljs
+++ src/cljs/sh.cljs
@@ -1,9 +1,15 @@
 (ns sh)
 
+(defn cwd []
+  (.cwd js/process))
+
 (defn cd [dir]
-  (letfn [(chdir [d] (.chdir js/process d))
-          (cwd [] (.cwd js/process))]
+  (letfn [(chdir [d] (.chdir js/process d))]
     (if (= (first dir) "/")
       (chdir dir)
       (chdir (str (cwd) "/" dir)))
     (cwd)))
+
+(defn ls [& [dir]]
+  (let [fs (js/require "fs")]
+    (vec (.readdirSync fs (str (cwd) "/" dir)))))
