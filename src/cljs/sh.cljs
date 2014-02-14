87ffb567b0b0823c0f2ae4460434e37d1cdcef4e
Max Kreminski <maxkreminski@gmail.com>
2014-02-14 08:40:25 -0800

    Expand tilde (~) character to homedir in sh functions

--- src/cljs/sh.cljs
+++ src/cljs/sh.cljs
@@ -1,21 +1,26 @@
 (ns sh
   (:require [clojure.browser.dom :as dom]
+            [clojure.string :as string]
             [goog.dom :as gdom]))
 
 (defn cwd []
   (.cwd js/process))
 
+(defn home []
+  (aget js/process "env" "HOME"))
+
 (defn absolute-path? [fpath]
   (= (first fpath) "/"))
 
 (defn expand-path [fpath]
-  (if (absolute-path? fpath)
-    fpath
-    (str (cwd) "/" fpath)))
+  (let [fpath (string/replace fpath #"~" (home))]
+    (if (absolute-path? fpath)
+      fpath
+      (str (cwd) "/" fpath))))
 
 (defn ls [& [dir]]
   (let [fs (js/require "fs")]
-    (vec (.readdirSync fs (expand-path dir)))))
+    (vec (.readdirSync fs (if dir (expand-path dir) (cwd))))))
 
 (defn cd [dir]
   (.chdir js/process (expand-path dir))
