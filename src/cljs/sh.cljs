3a480bb42a5b57d326720e544fb4ab8cfb0f69ea
Max Kreminski <maxkreminski@gmail.com>
2014-02-14 08:27:10 -0800

    Update cwd sidebar panel on working directory change

--- src/cljs/sh.cljs
+++ src/cljs/sh.cljs
@@ -1,15 +1,27 @@
-(ns sh)
+(ns sh
+  (:require [clojure.browser.dom :as dom]
+            [goog.dom :as gdom]))
 
 (defn cwd []
   (.cwd js/process))
 
-(defn cd [dir]
-  (letfn [(chdir [d] (.chdir js/process d))]
-    (if (= (first dir) "/")
-      (chdir dir)
-      (chdir (str (cwd) "/" dir)))
-    (cwd)))
+(defn absolute-path? [fpath]
+  (= (first fpath) "/"))
+
+(defn expand-path [fpath]
+  (if (absolute-path? fpath)
+    fpath
+    (str (cwd) "/" fpath)))
 
 (defn ls [& [dir]]
   (let [fs (js/require "fs")]
-    (vec (.readdirSync fs (str (cwd) "/" dir)))))
+    (vec (.readdirSync fs (expand-path dir)))))
+
+(defn cd [dir]
+  (.chdir js/process (expand-path dir))
+  (set! (.-innerHTML (.querySelector js/document "#cwd .wd")) (cwd))
+  (let [files (.querySelector js/document "#cwd .columns-list")]
+    (gdom/removeChildren files)
+    (doseq [file (ls)]
+      (dom/append files (dom/element [:li {} file]))))
+  (cwd))
